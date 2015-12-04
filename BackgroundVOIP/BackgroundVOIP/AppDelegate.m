f7fd88a8d32f7ebed9e2ea23a48e20d9c2837ebe
Jordan Zucker <jordan.zucker@gmail.com>
2015-12-04 12:09:47 -0800

    Basic pubnub subscription, and added VOIP background mode

--- BackgroundVOIP/BackgroundVOIP/AppDelegate.m
+++ BackgroundVOIP/BackgroundVOIP/AppDelegate.m
@@ -6,10 +6,12 @@
 //  Copyright © 2015 pubnub. All rights reserved.
 //
 
+#import <PubNub/PubNub.h>
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
-
+@property (nonatomic, strong) dispatch_queue_t pubNubQueue;
+- (void)startPubNub;
 @end
 
 @implementation AppDelegate
@@ -17,29 +19,44 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.pubNubQueue = dispatch_queue_create("com.PubNub.callbackQueue", DISPATCH_QUEUE_SERIAL);
+    self.client = [PubNub clientWithConfiguration:[PNConfiguration configurationWithPublishKey:@"demo-36" subscribeKey:@"demo-36"] callbackQueue:self.pubNubQueue];
+    
     return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    NSLog(@"%s", __PRETTY_FUNCTION__);
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    NSLog(@"%s", __PRETTY_FUNCTION__);
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    NSLog(@"%s", __PRETTY_FUNCTION__);
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    NSLog(@"%s", __PRETTY_FUNCTION__);
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    NSLog(@"%s", __PRETTY_FUNCTION__);
+}
+
+- (void)startPubNub {
+    if ([self.client isSubscribedOn:@"a"]) {
+        return;
+    }
+    [self.client subscribeToChannels:@[@"a"] withPresence:YES];
 }
 
 @end
